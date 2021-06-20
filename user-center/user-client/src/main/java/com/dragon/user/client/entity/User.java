f0069ef07f584325f309125afc8ef96f40f795da
chenxiaolong <chenxiaolong@gildata.com>
2021-06-21 00:29:25 +0800

    code generator

---
+++ user-center/user-client/src/main/java/com/dragon/user/client/entity/User.java
@@ -0,0 +1,68 @@
+package com.dragon.user.client.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.dragon.base.BaseEntity;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+import java.util.Date;
+
+/**
+ * <p>
+ * 系统用户
+ * </p>
+ *
+ * @author chenxiaolong
+ * @since 2021-06-21
+ */
+@Data
+@EqualsAndHashCode(callSuper = true)
+@Accessors(chain = true)
+@TableName("sys_user")
+public class User extends BaseEntity {
+
+    private static final long serialVersionUID=1L;
+
+    @TableId(value = "user_id", type = IdType.AUTO)
+    private Long userId;
+
+    /**
+     * 用户名
+     */
+    private String username;
+
+    /**
+     * 密码
+     */
+    private String password;
+
+    /**
+     * 邮箱
+     */
+    private String email;
+
+    /**
+     * 手机号
+     */
+    private String mobile;
+
+    /**
+     * 状态  0：禁用   1：正常
+     */
+    private Integer status;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 修改时间
+     */
+    private Date updateTime;
+
+
+}
